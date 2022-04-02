cce35f8a5e60c69269d63c0b292dba2f2d340a92
yhsanave <yhsanave@users.noreply.github.com>
2022-04-02 10:39:34 -0500

    listen I haven't touched this code in 9 months

--- cogs/makeballot.py
+++ cogs/makeballot.py
@@ -17,22 +17,23 @@ class MakeBallot(commands.Cog):
             return ctx.message.author == u
 
         async def send_preview():
-                    embed = discord.Embed(description = description.content, color = discord.Color.dark_red())
-                    embed.set_author(name=title.content)
-                    embed.set_footer(text=footer.content)
-                    embed.add_field(name='Ballot:', value=ballot.content, inline=False)
+            embed = discord.Embed(description = description.content, color = discord.Color.dark_red())
+            embed.set_author(name=title.content)
+            embed.set_footer(text=footer.content)
+            embed.add_field(name='Ballot:', value=ballot.content, inline=False)
 
-                    preview = await ctx.send(embed=embed)
-                    await preview.add_reaction('👍')
-                    await preview.add_reaction('👎')
-                    confirm = await self.bot.wait_for('reaction_add', check=checkr)
-                    if str(confirm[0].emoji) == '👍':
-                        channel = self.bot.get_channel(int(chan[2:-1]))
-                        await ctx.send(f'Confirmed, sending in #{channel}')
-                        await channel.send(embed = embed)
-                        await channel.send(content='@everyone',delete_after=.01)
-                    else:
-                        await ctx.send('Announcement Cancelled')
+            preview = await ctx.send(embed=embed)
+            await preview.add_reaction('👍')
+            await preview.add_reaction('👎')
+            confirm = await self.bot.wait_for('reaction_add', check=checkr)
+            if str(confirm[0].emoji) == '👍':
+                channel = self.bot.get_channel(int(chan[2:-1]))
+                await ctx.send(f'Confirmed, sending in #{channel}')
+                await channel.send(embed = embed)
+                await channel.send(content='@everyone',delete_after=.01)
+            else:
+                await ctx.send('Announcement Cancelled')
+
         if importCode:
             try: 
                 imported = json.loads(importCode)
@@ -40,7 +41,7 @@ class MakeBallot(commands.Cog):
                 description = imported['body']
                 ballot = '\n'.join(imported['shows'])
                 footer = imported['footer']
-                send_preview()
+                await send_preview()
             except json.JSONDecodeError:
                 await ctx.send('Invalid import code')
         else:
@@ -56,7 +57,7 @@ class MakeBallot(commands.Cog):
             await ctx.send('Enter Closing Statement:')
             footer = await self.bot.wait_for('message',check=check)
 
-            send_preview()
+            await send_preview()
 
 def setup(bot):
     bot.add_cog(MakeBallot(bot))
